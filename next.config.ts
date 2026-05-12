bacfbad1dbb34300d6e7e673b0efb8387c9d490e
Miguel Leal <miguellealw@gmail.com>
2026-05-11 21:55:13 -0500

    Removing nextjs optimization since YouTube CDN is already optimizing images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { hostname: "i.ytimg.com" },
       { hostname: "yt3.ggpht.com" },
